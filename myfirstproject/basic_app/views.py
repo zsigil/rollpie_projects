26062e20fad3f18b8848c3d6329a8b776ac6ea88
zsigil <chickpox@freemail.hu>
2018-07-06 08:55:25 +0200

    myfirstproject templates

--- myfirstproject/basic_app/views.py
+++ myfirstproject/basic_app/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 def index(request):
-    return HttpResponse("My first page in Django")
+    return render(request, 'basic_app/index.html')
