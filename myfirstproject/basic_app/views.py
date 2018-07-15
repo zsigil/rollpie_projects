ea92a0ed20366c62608bb8fc26b81b592bcde731
zsigil <chickpox@freemail.hu>
2018-07-15 11:57:44 +0200

    use of generic ListView

--- myfirstproject/basic_app/views.py
+++ myfirstproject/basic_app/views.py
@@ -1,8 +1,18 @@
 #views.py
 
-from django.shortcuts import render
 from .models import School
+from django.views.generic.list import ListView
 
-def index(request):
-    schools = School.objects.all()
-    return render(request, 'basic_app/index.html', {'schools':schools})
+class SchoolListView(ListView):
+    model = School
+    template_name = 'basic_app/index.html'
+    context_object_name = 'schools'
+
+    def get_queryset(self):
+        #return School.objects.filter(location__contains='New York')
+        return School.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super(SchoolListView, self).get_context_data()
+        context['some_data'] = 'This is some other data'
+        return context
--- myfirstproject/myfirstproject/urls.py
+++ myfirstproject/myfirstproject/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from basic_app import views
+from basic_app.views import SchoolListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', SchoolListView.as_view(), name='index')
 ]
