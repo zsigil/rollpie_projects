196cf251d1566125aed79dbff03d0e7af49f751f
zsigil <chickpox@freemail.hu>
2018-11-26 17:17:44 +0100

    customuser, profile models added

--- customuser_project/accounts/models.py
+++ customuser_project/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+
+class CustomUser(AbstractUser):
+    pass
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    pet_name = models.CharField(max_length=50, null=True, blank=True)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
